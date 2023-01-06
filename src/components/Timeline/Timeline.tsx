d7ed106b98be150940f51b55ac0d3c93a41e4618
Hamzah Ahmed <hamzah@syedahmed.net>
2023-01-06 23:33:15 +1100

    feat: more controls

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,5 +1,13 @@
-import { Flex, IconButton } from "@chakra-ui/react";
-import { Pause, Play } from "phosphor-react";
+import { Flex, IconButton, useToken } from "@chakra-ui/react";
+import {
+  Pause,
+  Play,
+  Repeat,
+  SkipBack,
+  SkipForward,
+  SpeakerHigh,
+  SpeakerSlash,
+} from "phosphor-react";
 import { useState } from "react";
 
 export default function Pane({
@@ -8,6 +16,11 @@ export default function Pane({
   onPlayingChange: (isPlaying: boolean) => void;
 }) {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const [isLooping, setIsLooping] = useState(false);
+
+  const yellow = useToken("colors", "yellow.500");
+
   return (
     <Flex
       direction="column"
@@ -15,7 +28,28 @@ export default function Pane({
       borderTop={"solid 1px gray"}
       w="full"
     >
-      <Flex w="full" justify={"space-around"}>
+      <Flex w="full" justify={"center"}>
+        <IconButton
+          aria-label="Toggle mute"
+          icon={
+            isMuted ? (
+              <SpeakerSlash weight="fill" color="gray" />
+            ) : (
+              <SpeakerHigh weight="fill" />
+            )
+          }
+          variant="ghost"
+          colorScheme={"gray"}
+          onClick={() => {
+            setIsMuted(!isMuted);
+          }}
+        />
+        <IconButton
+          aria-label="Skip back"
+          icon={<SkipBack weight="fill" />}
+          variant="ghost"
+          colorScheme={"gray"}
+        />
         <IconButton
           aria-label="Play"
           icon={isPlaying ? <Pause weight="fill" /> : <Play weight="fill" />}
@@ -24,6 +58,21 @@ export default function Pane({
           onClick={() => {
             onPlayingChange(!isPlaying);
             setIsPlaying(!isPlaying);
+          }}
+        />
+        <IconButton
+          aria-label="Skip forward"
+          icon={<SkipForward weight="fill" />}
+          variant="ghost"
+          colorScheme={"gray"}
+        />
+        <IconButton
+          aria-label="Repeat"
+          icon={<Repeat weight="fill" color={isLooping ? yellow : undefined} />}
+          variant="ghost"
+          colorScheme={"gray"}
+          onClick={() => {
+            setIsLooping(!isLooping);
           }}
         />
       </Flex>
