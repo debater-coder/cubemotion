195f768d7c39e7969247e42753c488bf9ab046de
Hamzah Ahmed <hamzah@syedahmed.net>
2023-01-06 17:27:29 +1100

    feat: player

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import TwistyPuzzle from "./components";
 
 function App() {
   return (
-    <>
-      <Button>Hi</Button>
-    </>
+    <Flex w="full" h="100vh" align="center" direction="column">
+      <Box w="full" h="50%">
+        <Canvas flat style={{ background: "#000000" }}>
+          <Environment preset="city" />
+          <OrbitControls />
+          <TwistyPuzzle alg="R U R' U'" hintFacelets="none" />
+        </Canvas>
+      </Box>
+    </Flex>
   );
 }
 
